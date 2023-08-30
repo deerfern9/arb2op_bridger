c3f07ab114f791e122931cc4301ae241f341d2d5
Fern <81830808+deerfern9@users.noreply.github.com>
2023-08-30 15:43:24 +0300

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,11 +1,22 @@
-import random
 import time
+import random
 from web3 import Web3
 from datetime import datetime
+from colorama import Fore, init
 
 from config import *
+init()
 
-web3 = Web3(Web3.HTTPProvider(arbitrum_rpc_url))
+colors = {
+    'time': Fore.MAGENTA,
+    'account_info': Fore.CYAN,
+    'message': Fore.BLUE,
+    'error_message': Fore.RED,
+    'reset': Fore.RESET
+}
+
+web3 = Web3(Web3.HTTPProvider(arbitrum_rpc))
+eth_web3 = Web3(Web3.HTTPProvider(ethereum_rpc))
 
 stargate_router_contract = web3.eth.contract(
     address=web3.to_checksum_address(stargate_arbitrum_address),
@@ -29,6 +40,11 @@ def read_file(filename):
 def write_to_file(filename, text):
     with open(filename, 'a') as file:
         file.write(f'{text}\n')
+
+
+def new_print(message_type, message, is_error=False):
+    print(f'{colors["time"]}{datetime.now().strftime("%d %H:%M:%S")}{colors["account_info"]} | {message_type} |'
+          f' {colors[(["message", "error_message"])[is_error]]}{message}{colors["reset"]}')
 
 
 def get_balance(address):
@@ -67,18 +83,31 @@ def bridge_arbitrum_optimism(account, amount):
 def main():
     privates = read_file('privates.txt')
     for private in privates:
-        account = web3.eth.account.from_key(private)
-        print(f'{datetime.now().strftime("%d %H:%M:%S")} | {account.address} | '
-              f'Swapping ETH from Arbitrum to Optimism...')
+        eth_gwei = web3.from_wei(eth_web3.eth.gas_price, 'gwei')
+        print(eth_gwei)
+        while eth_gwei > max_gwei:
+            new_print('INFO', f"Current gas fee {eth_gwei} gwei > {max_gwei} gwei. Waiting for 17 seconds...")
+            time.sleep(17)
+
         try:
-            amount_to_bridge = int((get_balance(account.address) - Web3.to_wei(0.0008, 'ether')) * percent_to_bridge / 100)
+            account = web3.eth.account.from_key(private)
+            new_print(account.address, f'Swapping ETH from Arbitrum to Optimism...')
+        except Exception as e:
+            new_print('INFO', f'Error: {e}', is_error=True)
+            continue
+
+        try:
+            amount_to_bridge = int((get_balance(account.address) - Web3.to_wei(0.0006, 'ether')) * percent_to_bridge / 100)
+            if amount_to_bridge < 0:
+                new_print(account.address, 'Insufficient balance', is_error=True)
+                write_to_file('insufficient_balance.txt', account.key)
+                continue
             bridge_txs_hash = bridge_arbitrum_optimism(account=account, amount=amount_to_bridge)
-            print(f'{datetime.now().strftime("%d %H:%M:%S")} | {account.address} | Bridge transaction hash: '
-                  f'{bridge_txs_hash.hex()}')
+            new_print(account.address, f'Bridge transaction hash: {bridge_txs_hash.hex()}')
             write_to_file('hashes.txt', f'{private};{account.address};{bridge_txs_hash.hex()}')
             time.sleep(random.randint(*wallets_delay))
         except Exception as e:
-            print(f'{datetime.now().strftime("%d %H:%M:%S")} | {account.address} | {e}')
+            new_print(account.address, f'Error: {e}', is_error=True)
             write_to_file('errors.txt', f'{private};{e}')
 
 
